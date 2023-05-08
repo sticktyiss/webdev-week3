3eb997ac6708828a43cfaea885877c7077566b8d
Tyiss Goodman <sticktyiss@gmail.com>
2023-05-08 11:16:26 -0700

    Fixed link connectivity to be functional

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import HomeScreen from "./components/homeComponents/HomeScreen";
+import NewRecipeScreen from "./components/newRecipeComponents/NewRecipeScreen";
+import DetailScreen from "./components/detailComponents/DetailScreen";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (

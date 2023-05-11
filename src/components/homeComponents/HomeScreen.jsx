64ce4880840daeaf71598644ee122cc189521fdd
Tyiss Goodman <sticktyiss@gmail.com>
2023-05-11 14:04:21 -0700

    Finished styling homescreen and recipe cards

--- src/components/homeComponents/HomeScreen.jsx
+++ src/components/homeComponents/HomeScreen.jsx
@@ -1,28 +1,53 @@
-import React, {useEffect} from 'react'
-import AdBanner from './AdBanner'
-import RecipeCard from './RecipeCard'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import AdBanner from "./AdBanner";
+import RecipeCard from "./RecipeCard";
+import axios from "axios";
+import { BiSearchAlt2 } from "react-icons/bi";
+import './HomeScreen.css'
 
-const HomeScreen = () => {  
+const HomeScreen = () => {
   const getRecipes = () => {
-    axios.get("https://recipes.devmountain.com")
-      .then((res) => {
-        // setRecipes(res.data)
-        console.log(res.data)
-      })
-  }
+    axios.get("https://recipes.devmountain.com").then((res) => {
+      // setRecipes(res.data)
+      console.log(res.data);
+    });
+  };
 
   useEffect(() => {
-    getRecipes()
-  }, [])
+    getRecipes();
+  }, []);
+
+  const [search, setSearch] = useState("");
+
+  // const handleChange = (e) => {
+  //   e.preventDefault()
+  //   setSearchInput(e.target.value)
+  // }
+  // if (searchInput.length > 0) {
+  //   .filter(() => {
+  //     return
+  //   })
+  // }
 
   return (
     <div>
       <AdBanner />
+      {
+        <span>
+          <BiSearchAlt2 size="2em" color="#DA7635" />
+          <input
+            type="text"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="Search for a Recipe"
+          />
+        </span>
+      }
+      <div id='recipes'>
       <RecipeCard />
-      {/* Much code from Part 2 will be placed around here. Do your best! */}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
